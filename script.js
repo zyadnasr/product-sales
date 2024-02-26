a5214815b2d9753d3dd6e8a1c5c22534773af6ad
ZYAD NASR <156793692+zyadnasr@users.noreply.github.com>
2024-02-26 21:17:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+
+function phones(phone){
+    img.src = phone;
+}
+
+
+function phonePic(color){
+    container.style.background = color;
+}
